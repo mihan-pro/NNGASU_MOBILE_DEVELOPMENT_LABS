a97b984c59f030c7cf7d9fff6ad181c0aa5cd03c
mihan-pro <mihan-pro@mail.ru>
2022-05-09 16:35:02 +0400

    feat: lab work 8: CHART

--- app/src/main/java/com/app/proshin934_1/MainActivity.java
+++ app/src/main/java/com/app/proshin934_1/MainActivity.java
@@ -1,14 +1,48 @@
 package com.app.proshin934_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.achartengine.ChartFactory;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 
+public class MainActivity extends Activity {
+    private static final int TEXT_SIZE = 40;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = buildIntent();
+        startActivity(intent);
+    }
+
+    public Intent buildIntent() {
+        int[] values = new int[] { 25,25,25,25 };
+        String[] bars = new String[] {"Bananas",  "Kiwi", "Oranges", "Cream"};
+        int[] colors = new int[] { Color.YELLOW,  Color.GREEN, Color.RED, Color.LTGRAY };
+
+        CategorySeries series = new CategorySeries("Pie Chart");
+        DefaultRenderer dr = new DefaultRenderer();
+
+        for (int v=0; v<4; v++){
+            series.add(bars[v], values[v]);
+            SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+            r.setColor(colors[v]);
+            dr.addSeriesRenderer(r);
+        }
+        dr.setZoomButtonsVisible(true);
+        dr.setZoomEnabled(true);
+        dr.setChartTitleTextSize(20);
+        dr.setLegendTextSize(TEXT_SIZE);
+        dr.setChartTitleTextSize(20);
+        dr.setZoomButtonsVisible(false);
+        dr.setLabelsTextSize(TEXT_SIZE);
+        dr.setLegendTextSize(TEXT_SIZE);
+        dr.setLabelsColor(Color.BLACK);
+        dr.setBackgroundColor(Color.BLACK);
+        return ChartFactory.getPieChartIntent(this, series, dr, "Fruit Salad");
     }
 }
